709cf05cc16c8400171e4f968bc8c7d90f16e1ef
fiona <fionamiaojing@gmail.com>
2018-06-17 18:31:43 -0700

    created dockerfile and some env var

--- server.js
+++ server.js
@@ -8,15 +8,26 @@ const apiProxy = httpProxy.createProxyServer();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/listing/:id', express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.redirect('/listing/1001');
+});
 
-const ServerOne = 'http://localhost:3001',
-      ServerTwo = 'http://localhost:3002',
+const ServerOne = 'http://itsyphotos-env.hksp68msdv.us-east-1.elasticbeanstalk.com',
+        // ServerOne = 'http://localhost:3001',
+        // ServerTwo = 'http://localhost:3002',
+      ServerTwo = 'http://itsy-env.zxd4jnvtyb.us-west-1.elasticbeanstalk.com',
+      //ServerThree = 'http://localhost:3003'; // for docker
       ServerThree = 'http://localhost:3003';
- 
+      //ServerThree = 'http://itsypurchasesidebar-env-1.mwfwxntniq.us-west-1.elasticbeanstalk.com/listing/1001/'; // for EBS
+
 app.all("/images/*", function(req, res) {
+    // console.log('redirecting to Server1');
+    apiProxy.web(req, res, {target: ServerOne});
+});
+app.all("/photo-gallery.js", function(req, res) {
     // console.log('redirecting to Server1');
     apiProxy.web(req, res, {target: ServerOne});
 });
@@ -26,8 +37,18 @@ app.all("/api/*", function(req, res) {
     apiProxy.web(req, res, {target: ServerTwo});
 });
 
+app.all("/bundle.js", function(req, res) {
+    // console.log('redirecting to Server2');
+    apiProxy.web(req, res, {target: ServerTwo});
+});
+
 app.all("/purchase/*", function(req, res) {
-    // console.log('redirecting to Server3');
+    console.log('redirecting to Server3', req.url);
+    apiProxy.web(req, res, {target: ServerThree});
+});
+
+app.all("/app.js", function(req, res) {
+    console.log('redirecting to Server3', req.url);
     apiProxy.web(req, res, {target: ServerThree});
 });
 
